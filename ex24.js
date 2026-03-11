f47351c27c4911b8efd8cdc5198313fe995a4600
JulinhoGamePlay <petitojulia839@gmail.com>
2026-03-11 16:28:50 -0300

    Update ex24.js

--- ex24.js
+++ ex24.js
@@ -1,20 +1,20 @@
 var prompt = require('prompt-sync')();
 
-let i = 1;
-let numero = +prompt('digita um numero para contar do 1 até ele: ')
+let i = 1; // de onde deve começar a contagem
+let numero = +prompt('digita um numero para contar do 1 até ele: ') //pedir até onde o usuario quer que vá a contagem
 
-if (numero > 0) {
-    while (i <= numero ) {
-        console.log(i)
-        i++;
+if (numero > 0) { //verificar se o numero escolhido pelo usuario é positivo
+    while (i <= numero ) { //fazer enquanto o i(1) for menor que o numero escolhido pelo usuario
+        console.log(i) //mostrar o valor atual de i
+        i++; //aumentar em 1 o valor de i
     }
-} else if (numero < 0) {
-    while (i >= numero) {
-        console.log(i)
-        i--;
+} else if (numero < 0) { //verificar se o numero escolhido pelo usuario é negativo
+    while (i >= numero) { //fazer enquanto o i(1) for maior que o numero escolhido pelo usuario
+        console.log(i) //mostrar o valor atual de i
+        i--; //diminuir em 1 o valor de i
     }
 } else {
-    console.log('o numero escolhido foi 0')
+    console.log('o numero escolhido foi 0/') //apenas mostrar essa mensagem caso o valor escolhido seja 0
 }
 
-console.log('fim do loop');
+console.log('fim do loop'); //mensagem quando acabar as contagens
